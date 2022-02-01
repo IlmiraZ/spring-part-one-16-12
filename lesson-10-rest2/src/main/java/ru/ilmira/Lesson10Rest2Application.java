a6ebb4da19d19be2a534f868983a142acbc68581
Ilmira <ilmira.82@inbox.ru>
2022-02-01 21:24:36 +0300

    lesson №10 init

---
+++ lesson-10-rest2/src/main/java/ru/ilmira/Lesson10Rest2Application.java
@@ -0,0 +1,13 @@
+package ru.ilmira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson10Rest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson10Rest2Application.class, args);
+    }
+
+}
